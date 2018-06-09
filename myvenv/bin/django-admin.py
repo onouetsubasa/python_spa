49223fdf9b45d77724ebc32aa81815eae5a2f35b
onouetsubasa <onue.wing0412@gmail.com>
2018-06-09 13:36:02 +0900

    環境設定

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tsubasa/development/queue_project/python_spa/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
