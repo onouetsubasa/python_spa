879efd75900504d6436675667b88afb639faf443
onouetsubasa <onoue.wing0412@gmail.com>
2018-06-10 00:34:53 +0900

    通信で取得

---
+++ frontend/src/components/Poll/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-card>
+      <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs6 v-for="data in questions" :key="data.id">
+            <v-card color="primary" class="white--text">
+              <v-card-title primary-title>
+                <div>{{ data.questionText }}</div>
+              </v-card-title>
+              <v-card-text>
+                <div>{{ data.pubDate|printDate }}</div>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import moment from 'moment'
+
+export default {
+  name: 'PollIndex',
+
+  data () {
+    return {
+      questions: [],
+    }
+  },
+
+  methods: {
+    fetchData () {
+      axios.get('http://localhost:8000/api/1.0/questions/').then(res => {
+
+        this.questions = res.data.results
+      })
+    },
+  },
+
+  mounted () {
+    this.fetchData()
+  },
+
+  filters: {
+    printDate (val) {
+      return moment(val).locale('ja').format('YYYY年MM月DD日(ddd) HH時mm分ss秒')
+    },
+  },
+}
+</script>
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'polls',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
@@ -70,6 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
@@ -120,3 +124,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# =========================
+# django-restframework 設定
+# =========================
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+
+}
